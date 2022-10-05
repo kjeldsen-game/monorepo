139688f39572f4f3dc7aa7ed9b3113b90808ff3e
Ivan <ivangonzalezcabral94@gmail.com>
2022-10-05 17:31:01 +0100

    added gateway service

---
+++ services/gateway-service/infrastructure/src/main/java/com/kjeldsen/gateway/configuration/GatewaySecurityConfig.java
@@ -0,0 +1,27 @@
+package com.kjeldsen.gateway.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+@EnableResourceServer
+public class GatewaySecurityConfig extends ResourceServerConfigurerAdapter {
+
+    private static final String[] AUTH_WHITELIST = {
+        "/auth-service/v1/sign-up",
+        "/auth-service/oauth/token",
+        "/auth-service/oauth/check_token"
+    };
+
+    @Override
+    public void configure(final HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .antMatchers(AUTH_WHITELIST)
+            .permitAll()
+            .antMatchers("/**")
+            .authenticated();
+    }
+
+}
