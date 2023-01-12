73f407be4483982e7899bbfbedb818ea335c3d37
Ivan <ivangonzalezcabral94@gmail.com>
2023-01-12 16:52:08 +0000

    added kafka service addition of topics

--- services/kafka-service/src/adapter_file_system/main.rs
+++
@@ -1,6 +0,0 @@
-use std::fs;
-
-pub fn read_file_to_string(path: String) -> String {
-    fs::read_to_string(path)
-        .expect("Valid file path")
-}
--- services/kafka-service/src/main.rs
+++ services/kafka-service/src/main.rs
@@ -1,28 +1,83 @@
-use std::fmt::Error;
-use dotenv::dotenv;
 use std::env;
 use std::fmt;
+use std::fmt::Error;
+use std::fs;
 
-use kafka::client::KafkaClient;
+use dotenv::dotenv;
+
+use serde::{Deserialize, Serialize};
+
 use kafka::client::metadata::TopicNames;
-
-use rskafka::client::ClientBuilder;
+use kafka::client::KafkaClient;
 use rskafka::client::controller::ControllerClient;
+use rskafka::client::ClientBuilder;
 
-use serde::{Serialize, Deserialize};
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    dotenv().ok(); // Load environment variables
 
-mod adapter_file_system;
+    let kafka_connection = env::var("KAFKA_HOST_AND_PORT").unwrap().to_string();
+    let topics_file_path = env::var("TOPICS_FILE_PATH").unwrap().to_string();
 
-use adapter_file_system::main::read_file_to_string;
+    let rs_kafka_client = ClientBuilder::new(vec![kafka_connection.clone()])
+        .build()
+        .await
+        .unwrap();
+
+    let controller_client: ControllerClient = rs_kafka_client.controller_client().unwrap();
+
+    let mut kafka_client = KafkaClient::new(vec![kafka_connection.to_owned()]);
+
+    let json_file = fs::read_to_string(topics_file_path).unwrap();
+    let topic_file: TopicFile = serde_json::from_str(&json_file).unwrap();
+
+    for topic in topic_file.topics {
+        kafka_client.load_metadata_all().unwrap();
+        let current_topics = kafka_client.topics();
+        let mut current_topic_names: TopicNames = current_topics.names();
+        update_topic(&controller_client, &mut current_topic_names, &topic).await;
+    }
+
+    Ok(())
+}
+
+async fn update_topic<'a>(
+    controller_client: &'a ControllerClient,
+    _current_topic_names: &'a mut TopicNames<'_>,
+    topic: &'a Topic,
+) {
+    if _current_topic_names.find(|topic_name| topic_name.eq(&topic.name)) == None {
+        println!("Topic {} to be created...", topic);
+        create_topic(&controller_client, &topic).await;
+        println!("Topic {} created...", topic);
+    } else {
+        println!("Topic {} already exists, ignored.", topic);
+    }
+}
+
+async fn create_topic<'a>(controller_client: &'a ControllerClient, topic: &'a Topic) {
+    controller_client
+        .create_topic(
+            &topic.name,
+            topic.num_partitions,
+            topic.replication_factor,
+            topic.timeout_ms,
+        )
+        .await
+        .unwrap();
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 struct TopicFile {
-    topics: Vec<Topic>
+    topics: Vec<Topic>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Topic {
-    name: String
+    name: String,
+    num_partitions: i32,
+    replication_factor: i16,
+    timeout_ms: i32,
 }
 
 impl fmt::Display for Topic {
@@ -30,52 +85,3 @@ impl fmt::Display for Topic {
         write!(f, "{}", self.name)
     }
 }
-
-
-#[tokio::main]
-async fn main() -> Result<(), Error> {
-
-    dotenv().ok(); // Load environment variables
-
-    let kafka_connection = env::var("KAFKA_HOST_AND_PORT").unwrap().to_string();
-
-    let rs_kafka_client = ClientBuilder::new(vec![kafka_connection.clone()]).build().await.unwrap();
-
-    let controller_client: ControllerClient = rs_kafka_client.controller_client().unwrap();
-
-    let mut kafka_client = KafkaClient::new(vec!(kafka_connection.to_owned()));
-
-
-    let json_file = read_file_to_string("topics.json".to_string());
-
-    let topic_file: TopicFile = serde_json::from_str(&json_file).unwrap();
-
-    for topic in topic_file.topics {
-        kafka_client.load_metadata_all().unwrap();
-        let current_topics = kafka_client.topics();
-        let mut current_topic_names: TopicNames = current_topics.names();
-        update_topic(&controller_client, &mut current_topic_names, &topic.to_string()).await;
-    }
-
-    Ok(())
-}
-
-async fn update_topic<'a>(controller_client:  &'a ControllerClient, _current_topic_names:  &'a mut TopicNames<'_>, topic: &'a String) {
-  
-    if _current_topic_names.find(|topic_name| topic_name.eq(topic)) == None {
-        println!("Topic {:?} to be created...", topic);
-        create_topic(&controller_client, &topic).await;
-    } else {
-        println!("Topic {:?} already exists", topic);
-    }
-}
-
-async fn create_topic<'a>(controller_client: &'a ControllerClient, topic: &'a String) {
-    controller_client.create_topic(
-        topic,
-        2,      // partitions
-        1,      // replication factor
-        5_000,  // timeout (ms)
-    ).await.unwrap();
-}
-
