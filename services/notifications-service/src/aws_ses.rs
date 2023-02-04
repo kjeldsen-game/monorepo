89a6b1fbc0aa90bce89020110d32f0536f6a7aef
Ivan <ivangonzalezcabral94@gmail.com>
2023-02-04 22:18:12 +0000

    wip template processing

--- services/notifications-service/src/aws_ses.rs
+++ services/notifications-service/src/aws_ses.rs
@@ -2,40 +2,62 @@ use aws_sdk_ses::error::{CreateTemplateError, UpdateTemplateError};
 use aws_sdk_ses::model::Template;
 use aws_sdk_ses::output::{CreateTemplateOutput, UpdateTemplateOutput};
 use aws_sdk_ses::types::SdkError;
-use aws_sdk_ses::{Client, Region};
+use aws_sdk_ses::Client;
 
-pub async fn send_email_from_template() {
-    let config_loader = aws_config::from_env()
-        .region(Region::new("eu-west-1"))
-        .endpoint_url(String::from("http://localhost:4566/"));
+use std::fs;
 
-    let shared_config = config_loader.load().await;
+use crate::aws_ses_client;
+use crate::ses_template;
 
-    println!(
-        "Loaded AWS config {:?},{:?}",
-        shared_config.endpoint_resolver(),
-        shared_config.endpoint_url()
-    );
-    let client = ses_client(&shared_config);
-
-    create_or_update_template(client).await;
+pub async fn upload_templates() {
+    let client = aws_ses_client::new().await;
+    process_templates(client).await;
 }
 
-fn ses_client(conf: &aws_config::SdkConfig) -> Client {
-    let mut ses_config_builder = aws_sdk_ses::config::Builder::from(conf);
-    if use_localstack() {
-        println!(
-            "Using localstack {}",
-            std::env::var("LOCALSTACK_ENDPOINT").unwrap().to_string()
-        );
-        ses_config_builder = ses_config_builder
-            .endpoint_url(std::env::var("LOCALSTACK_ENDPOINT").unwrap().to_string())
+async fn process_templates(client: Client) {
+    let templates_dir = fs::read_dir(String::from("resources/ses_templates")).unwrap();
+
+    for dir in templates_dir {
+        let template_file = fs::read_to_string(dir.unwrap().path()).unwrap();
+        let ses_template: ses_template::SESTemplate = serde_json::from_str(&template_file).unwrap();
+
+        create_or_update_template(&client, ses_template).await;
     }
-    Client::from_conf(ses_config_builder.build())
 }
 
-fn use_localstack() -> bool {
-    std::env::var("LOCALSTACK").unwrap_or_default() == "false"
+async fn create_or_update_template(client: &Client, ses_template: ses_template::SESTemplate) {
+    let template = Template::builder()
+        .template_name(ses_template.name)
+        .subject_part(ses_template.subject)
+        .text_part(ses_template.text)
+        .html_part(ses_template.html)
+        .build();
+
+    if let Err(error) = update_template(&client, &template).await {
+        let update_template_error = error.into_service_error();
+
+        println!("update template error {:?}", update_template_error);
+
+        if update_template_error.is_template_does_not_exist_exception() {
+            if let Err(ss) = create_template(&client, &template).await {
+                let create_template_error = ss.into_service_error();
+
+                panic!(
+                    "Problem creating template: {:?}",
+                    create_template_error.message()
+                )
+            } else {
+                println!("created template {:?}", template.template_name());
+            }
+        } else {
+            panic!(
+                "Problem updating template: {:?}",
+                update_template_error.message()
+            )
+        }
+    } else {
+        println!("updated template {:?}", template.template_name());
+    }
 }
 
 async fn create_template(
@@ -58,48 +80,4 @@ async fn update_template(
         .template(template.clone())
         .send()
         .await
-}
-
-async fn create_or_update_template(client: Client) {
-    let template = Template::builder()
-        .template_name(String::from("asdasd"))
-        .subject_part(String::from("asdasd"))
-        .text_part(String::from("asdasd"))
-        .html_part(String::from("asdasd"))
-        .build();
-
-    if let Err(error) = update_template(&client, &template).await {
-        let update_template_error = error.into_service_error();
-        if update_template_error.is_template_does_not_exist_exception() {
-            if let Err(ss) = create_template(&client, &template).await {
-                let create_template_error = ss.into_service_error();
-
-                panic!(
-                    "Problem creating template: {:?}",
-                    create_template_error.message()
-                )
-            } else {
-                println!("created template {:?}", template.template_name());
-            }
-        } else {
-            panic!(
-                "Problem updating template: {:?}",
-                update_template_error.message()
-            )
-        }
-    } else {
-        println!("updated template {:?}", template.template_name());
-    }
-    /*
-    match update_template(client, template).await {
-        Ok(output) => Ok(output),
-        Err(error) => {
-            if error
-                .into_service_error()
-                .is_template_does_not_exist_exception()
-            {
-                create_template(client, template).await;
-            }
-        }
-    }*/
 }
