d79bc3c2f7d76bcbfbae29d8cdf7865ec4b44ac9
Miguel Ignacio Cabrera San Gil <miguelcabsan@gmail.com>
2023-03-27 01:09:11 +0100

    chore: add lombok dependency

--- libs/event-lib/src/main/java/com/kjeldsen/events/Event.java
+++ libs/event-lib/src/main/java/com/kjeldsen/events/Event.java
@@ -1,25 +1,13 @@
 package com.kjeldsen.events;
 
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+
 import java.time.Instant;
 
+@SuperBuilder
+@Getter
 public abstract class Event {
     private final EventId id;
     private final Instant occurredAt;
-
-    protected Event() {
-        this.id = EventId.generate();
-        this.occurredAt = Instant.now();
-    }
-    protected Event(EventId id, Instant occurredAt) {
-        this.id = id;
-        this.occurredAt = occurredAt;
-    }
-
-    public EventId getId() {
-        return id;
-    }
-
-    public Instant getOccurredAt() {
-        return occurredAt;
-    }
 }
