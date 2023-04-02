866142e9ac37b59bef5a8c50797f9f4a2f39e38e
Carlos Baez Fumero <carlos.baez.fumero@gmail.com>
2023-04-02 20:58:02 +0200

    centered columns

--- frontends/beta-frontend/shared/components/Grid/Grid.tsx
+++ frontends/beta-frontend/shared/components/Grid/Grid.tsx
@@ -10,7 +10,7 @@ type GridProps<T> = {
 
 function Grid<T>({ rows, columns, onRowClick }: GridProps<T>) {
   return (
-    <Box sx={{ minWidth: '1400px' }}>
+    <Box sx={{ minWidth: '1200px' }}>
       <DataGrid autoHeight={true} rows={rows} columns={columns} hideFooter={true} onRowClick={onRowClick} />
     </Box>
   )
