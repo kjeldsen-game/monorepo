d2c56d8a008c5f4b2a2463deaf39b33aad6bf6b1
Miguel Ignacio Cabrera San Gil <miguelcabsan@gmail.com>
2023-04-18 23:33:29 +0100

    fix: fix team creation event cycle

--- services/player-service/adapter-kafka/src/main/java/com/kjeldsen/player/kafka/events/UserSignedUpEvent.java
+++ services/player-service/adapter-kafka/src/main/java/com/kjeldsen/player/kafka/events/UserSignedUpEvent.java
@@ -1,11 +1,17 @@
 package com.kjeldsen.player.kafka.events;
 
 import com.kjeldsen.events.Event;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 @Getter
+@Setter
 @SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserSignedUpEvent extends Event {
     private String userId;
 
