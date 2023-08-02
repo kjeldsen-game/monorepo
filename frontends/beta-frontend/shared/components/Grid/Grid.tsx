366643a5f72241a33e6c84ba274dd7c81ee7b567
Carlos Baez Fumero <carlos.baez.fumero@gmail.com>
2023-08-02 18:41:31 +0200

    refactored code to clean rowclick

--- frontends/beta-frontend/shared/components/Grid/Grid.tsx
+++ frontends/beta-frontend/shared/components/Grid/Grid.tsx
@@ -1,21 +1,23 @@
 import { DataGrid, GridCallbackDetails, GridColDef, GridEventListener, GridRowParams, MuiEvent } from '@mui/x-data-grid'
 import Box from '@mui/material/Box'
-import Link from 'next/link'
+import { useRouter } from 'next/router'
+
 
 type GridProps<T> = {
   rows: T[]
   columns: GridColDef[]
-  // handleRowClick?: (params: GridRowParams, event: MuiEvent, details: GridCallbackDetails) => void
-}
-
-const handleRowClick: GridEventListener<'rowClick'> = (params) => {
-  <Link href={`/players/${params.row.id}`}></Link>
-
-  console.log(params)
-  console.log(params.row.id)
 }
 
 function Grid<T>({ rows, columns }: GridProps<T>) {
+  const router = useRouter()
+
+  const handleRowClick: GridEventListener<'rowClick'> = (params) => {
+    router.push(`/player/${params.row.id}`)
+
+    console.log(params)
+    console.log(params.row.id)
+  }
+
   return (
     <Box>
       <DataGrid autoHeight={true} rows={rows} columns={columns} hideFooter={true} onRowClick={handleRowClick} />
