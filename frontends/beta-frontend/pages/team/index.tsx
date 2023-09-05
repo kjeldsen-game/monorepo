a4035483146fd471f34fb58001dd1e6d8559c09f
Carlos Baez Fumero <carlos.baez.fumero@gmail.com>
2023-09-05 11:44:21 +0200

    stashing comments for correction

--- frontends/beta-frontend/pages/team/index.tsx
+++ frontends/beta-frontend/pages/team/index.tsx
@@ -7,12 +7,14 @@ import PlayerTactics from '@/shared/components/PlayerTactics'
 import TeamTactics from '@/shared/components/TeamTactics'
 import { teamColumn } from '@/shared/components/Grid/TeamColumn'
 import useSWR from "swr"
+import { connectorAPI } from '@/libs/fetcher'
 
-const fetcher = (url: string) => fetch(url).then((res) => res.json());
-const API = "http://localhost:8082/player?size=40&page=0";
+// const fetcher = (url: string) => fetch(url).then((res) => res.json());
+// const API = "http://localhost:8082/player?size=40&page=0";
+const API = "/player?size=40&page=0";
 
 export async function getServerSideProps() {
-  const repoInfo = await fetcher(API);
+  const repoInfo = await connectorAPI(API, "GET");
   return {
     props: {
       fallback: {
@@ -28,10 +30,10 @@ interface TeamProps {
 
 const Team: NextPage<TeamProps> = ({fallback}) => {
   const { data, error } = useSWR(API);
-  console.log(fallback[API])
+  console.log('fallback[API] is', fallback[API])
 
   console.log("Is data ready?", !!data);
-  console.log(data)
+  console.log('data is', data)
 
   if (error) return <div>failed to load</div>
 
