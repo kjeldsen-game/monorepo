0baf4036ffea2ba5ed2b83b39e233bf245f6240a
Carlos Baez Fumero <carlos.baez.fumero@gmail.com>
2023-09-18 19:25:25 +0200

    feat: figured out issue AFAIK, cleaning up code to ask questions

--- frontends/beta-frontend/libs/fetcher/index.ts
+++ frontends/beta-frontend/libs/fetcher/index.ts
@@ -39,16 +39,13 @@ export const connector = async (url: string, method: string, body?: any) => {
   const response = await fetch(`${url}`, {
     method: method,
     headers: {
-      // "accept": "*/*",
       "Content-Type": "application/json",
     },
     body: JSON.stringify(body),
   });
   if (response.ok) {
-    console.log(response)
-    response.json().then((data) => console.log(data));
-    // const data = await response.json();
-    // return data;
+    const data = await response.json();
+    return data;
   } else {
     const error = await response.json();
     throw new Error(error.message);
