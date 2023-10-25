54043797dac3b90abcf1c2bf25509b3cedf10578
Carlos Baez Fumero <carlos.baez.fumero@gmail.com>
2023-10-25 12:17:48 +0200

    fixed box css props

--- frontends/beta-frontend/shared/components/Grid/Grid.tsx
+++ frontends/beta-frontend/shared/components/Grid/Grid.tsx
@@ -1,7 +1,5 @@
 import { DataGrid, GridCallbackDetails, GridColDef, GridEventListener, GridRowParams, MuiEvent } from '@mui/x-data-grid'
 import Box from '@mui/material/Box'
-import { useContext } from 'react'
-import css from 'styled-jsx/css'
 
 type GridProps<T> = {
   rows: T[]
@@ -11,33 +9,23 @@ type GridProps<T> = {
 function Grid<T>({ rows, columns }: GridProps<T>) {
   return (
     <Box sx={{
-      '& .super-app.goalkeeper .MuiDataGrid-cellContent': {
-        backgroundColor: '#A4BC10',
+      '& .super-app .MuiDataGrid-cellContent': {
         color: 'white',
         padding: '6px',
         fontWeight: '600',
         borderRadius: '8%',
+      },
+      '& .super-app.goalkeeper .MuiDataGrid-cellContent': {
+        backgroundColor: '#A4BC10',
       },
       '& .super-app.defender .MuiDataGrid-cellContent': {
         backgroundColor: '#F68B29',
-        color: 'white',
-        padding: '6px',
-        fontWeight: '600',
-        borderRadius: '8%',
       },
       '& .super-app.middle .MuiDataGrid-cellContent': {
         backgroundColor: '#29B6F6',
-        color: 'white',
-        padding: '6px',
-        fontWeight: '600',
-        borderRadius: '8%',
       },
       '& .super-app.forward .MuiDataGrid-cellContent': {
         backgroundColor: '#E52323',
-        color: 'white',
-        padding: '6px',
-        fontWeight: '600',
-        borderRadius: '8%',
       },
     }}>
       <DataGrid autoHeight={true} rows={rows} columns={columns} hideFooter={true} />
