ef2c05ecd975e93e5ccbd4c48be786d1d1d87afb
kdcr <kevincabrerarodriguez@gmail.com>
2024-02-02 21:53:30 +0000

    Add player position and order selectors

--- frontends/beta-frontend/shared/components/Grid/TeamColumn.tsx
+++ frontends/beta-frontend/shared/components/Grid/TeamColumn.tsx
@@ -1,8 +1,11 @@
-import { GridCellParams, GridValueFormatterParams, GridValueGetterParams } from '@mui/x-data-grid'
+import { GridCellParams, GridColDef, GridValueFormatterParams, GridValueGetterParams } from '@mui/x-data-grid'
 import { GridAlignment } from '@mui/x-data-grid'
 import Link from 'next/link'
 import { css } from '@emotion/react'
 import clsx from 'clsx'
+import { PlayerOrderSelect } from '../PlayerOrderSelect'
+import { PlayerPositionSelect } from '../PlayerPositionSelect'
+import { PlayerPosition } from '@/shared/models/PlayerPosition'
 
 const linkStyle = css`
   text-decoration: none;
@@ -12,258 +15,284 @@ const linkStyle = css`
   }
 `
 
-export const teamColumn = [
-  // { field: 'status', headerName: 'Health', headerAlign: 'center' as GridAlignment, align: 'center' as GridAlignment, minWidth: 70, flex: 1 },
-  {
-    field: 'name',
-    headerName: 'Name',
-    headerAlign: 'center' as GridAlignment,
-    minWidth: 130,
-    flex: 1,
-    renderCell: (params: GridCellParams) => (
-      <Link passHref href={`/player/${params.row.id}`}>
-        <a css={linkStyle}>{params.row.name}</a>
-      </Link>
-    ),
-  },
-  { field: 'age', headerName: 'Age', headerAlign: 'center' as GridAlignment, align: 'center' as GridAlignment, minWidth: 70, flex: 1 },
-  {
-    field: 'position',
-    valueFormatter: (params: GridValueFormatterParams<unknown>) => {
-      switch (params.value) {
-        case 'GOALKEEPER':
-          return 'GK'
-        case 'DEFENDER':
-          return 'DF'
-        case 'MIDDLE':
-          return 'MD'
-        case 'FORWARD':
-          return 'FW'
-        default:
-          return params.value
-      }
+export const teamColumn = (isEditing: boolean) => {
+  const columns: GridColDef[] = [
+    // { field: 'status', headerName: 'Health', headerAlign: 'center' as GridAlignment, align: 'center' as GridAlignment, minWidth: 70, flex: 1 },
+    {
+      field: 'name',
+      headerName: 'Name',
+      headerAlign: 'center' as GridAlignment,
+      minWidth: 130,
+      flex: 1,
+      renderCell: (params: GridCellParams) => (
+        <Link passHref href={`/player/${params.row.id}`}>
+          <a css={linkStyle}>{params.row.name}</a>
+        </Link>
+      ),
     },
-    headerName: 'Position',
-    headerAlign: 'center' as GridAlignment,
-    align: 'center' as GridAlignment,
-    minWidth: 70,
-    flex: 1,
-    // Assign Css class based on position
-    cellClassName: (params: GridCellParams<string>) => {
-      if (params.value == null) {
-        return ''
-      }
+    { field: 'age', headerName: 'Age', headerAlign: 'center' as GridAlignment, align: 'center' as GridAlignment, minWidth: 70, flex: 1 },
+    {
+      field: 'position',
+      valueFormatter: (params: GridValueFormatterParams<unknown>) => {
+        switch (params.value) {
+          case 'GOALKEEPER':
+            return 'GK'
+          case 'DEFENDER':
+            return 'DF'
+          case 'MIDDLE':
+            return 'MD'
+          case 'FORWARD':
+            return 'FW'
+          default:
+            return params.value
+        }
+      },
+      headerName: 'Position',
+      headerAlign: 'center' as GridAlignment,
+      align: 'center' as GridAlignment,
+      minWidth: 70,
+      flex: 1,
+      // Assign Css class based on position
+      cellClassName: (params: GridCellParams<string>) => {
+        if (params.value == null) {
+          return ''
+        }
 
-      return clsx('super-app', {
-        [params.value.toLowerCase()]: true,
-      })
+        return clsx('super-app', {
+          [params.value.toLowerCase()]: true,
+        })
+      },
     },
-  },
-  {
-    field: 'DEFENSIVE_POSITIONING',
-    renderHeader: () => (
-      <div>
-        DP<sup>CO</sup>
-      </div>
-    ),
-    minWidth: 50,
-    headerAlign: 'center' as GridAlignment,
-    align: 'center' as GridAlignment,
-    flex: 1,
-    valueGetter: (params: GridValueGetterParams) => {
-      return params.row.actualSkills.DEFENSIVE_POSITIONING?.PlayerSkills.actual || params.row.actualSkills.CONTROL?.PlayerSkills.actual
+    {
+      field: 'DEFENSIVE_POSITIONING',
+      renderHeader: () => (
+        <div>
+          DP<sup>CO</sup>
+        </div>
+      ),
+      minWidth: 50,
+      headerAlign: 'center' as GridAlignment,
+      align: 'center' as GridAlignment,
+      flex: 1,
+      valueGetter: (params: GridValueGetterParams) => {
+        return params.row.actualSkills.DEFENSIVE_POSITIONING?.PlayerSkills.actual || params.row.actualSkills.CONTROL?.PlayerSkills.actual
+      },
     },
-  },
-  {
-    field: 'BALL_CONTROL',
-    renderHeader: () => (
-      <div>
-        BC<sup>INT</sup>
-      </div>
-    ),
-    headerAlign: 'center' as GridAlignment,
-    align: 'center' as GridAlignment,
-    minWidth: 50,
-    flex: 1,
-    valueGetter: (params: GridValueGetterParams) => {
-      return params.row.actualSkills.BALL_CONTROL?.PlayerSkills.actual || params.row.actualSkills.INTERCEPTIONS?.PlayerSkills.actual
+    {
+      field: 'BALL_CONTROL',
+      renderHeader: () => (
+        <div>
+          BC<sup>INT</sup>
+        </div>
+      ),
+      headerAlign: 'center' as GridAlignment,
+      align: 'center' as GridAlignment,
+      minWidth: 50,
+      flex: 1,
+      valueGetter: (params: GridValueGetterParams) => {
+        return params.row.actualSkills.BALL_CONTROL?.PlayerSkills.actual || params.row.actualSkills.INTERCEPTIONS?.PlayerSkills.actual
+      },
     },
-  },
-  {
-    field: 'SCORE',
-    renderHeader: () => (
-      <div>
-        SC<sup>1on1</sup>
-      </div>
-    ),
-    headerAlign: 'center' as GridAlignment,
-    align: 'center' as GridAlignment,
-    minWidth: 50,
-    flex: 1,
-    valueGetter: (params: GridValueGetterParams) => {
-      return params.row.actualSkills.SCORING?.PlayerSkills.actual || params.row.actualSkills.ONE_ON_ONE?.PlayerSkills.actual
+    {
+      field: 'SCORE',
+      renderHeader: () => (
+        <div>
+          SC<sup>1on1</sup>
+        </div>
+      ),
+      headerAlign: 'center' as GridAlignment,
+      align: 'center' as GridAlignment,
+      minWidth: 50,
+      flex: 1,
+      valueGetter: (params: GridValueGetterParams) => {
+        return params.row.actualSkills.SCORING?.PlayerSkills.actual || params.row.actualSkills.ONE_ON_ONE?.PlayerSkills.actual
+      },
     },
-  },
-  {
-    field: 'PASSING',
-    renderHeader: () => (
-      <div>
-        PA<sup>ORG</sup>
-      </div>
-    ),
-    headerAlign: 'center' as GridAlignment,
-    align: 'center' as GridAlignment,
-    minWidth: 50,
-    flex: 1,
-    valueGetter: (params: GridValueGetterParams) => {
-      return params.row.actualSkills.PASSING?.PlayerSkills.actual || params.row.actualSkills.ORGANIZATION?.PlayerSkills.actual
+    {
+      field: 'PASSING',
+      renderHeader: () => (
+        <div>
+          PA<sup>ORG</sup>
+        </div>
+      ),
+      headerAlign: 'center' as GridAlignment,
+      align: 'center' as GridAlignment,
+      minWidth: 50,
+      flex: 1,
+      valueGetter: (params: GridValueGetterParams) => {
+        return params.row.actualSkills.PASSING?.PlayerSkills.actual || params.row.actualSkills.ORGANIZATION?.PlayerSkills.actual
+      },
     },
-  },
-  {
-    field: 'OFFENSIVE_POSITIONING',
-    renderHeader: () => (
-      <div>
-        OP<sup>POS</sup>
-      </div>
-    ),
-    headerAlign: 'center' as GridAlignment,
-    align: 'center' as GridAlignment,
-    minWidth: 50,
-    flex: 1,
-    valueGetter: (params: GridValueGetterParams) => {
-      return params.row.actualSkills.OFFENSIVE_POSITIONING?.PlayerSkills.actual || params.row.actualSkills.POSITIONING?.PlayerSkills.actual
+    {
+      field: 'OFFENSIVE_POSITIONING',
+      renderHeader: () => (
+        <div>
+          OP<sup>POS</sup>
+        </div>
+      ),
+      headerAlign: 'center' as GridAlignment,
+      align: 'center' as GridAlignment,
+      minWidth: 50,
+      flex: 1,
+      valueGetter: (params: GridValueGetterParams) => {
+        return params.row.actualSkills.OFFENSIVE_POSITIONING?.PlayerSkills.actual || params.row.actualSkills.POSITIONING?.PlayerSkills.actual
+      },
     },
-  },
-  {
-    field: 'TACKLING',
-    renderHeader: () => (
-      <div>
-        TA<sup>RE</sup>
-      </div>
-    ),
-    headerAlign: 'center' as GridAlignment,
-    align: 'center' as GridAlignment,
-    minWidth: 50,
-    flex: 1,
-    valueGetter: (params: GridValueGetterParams) => {
-      return params.row.actualSkills.TACKLING?.PlayerSkills.actual || params.row.actualSkills.REFLEXES?.PlayerSkills.actual
+    {
+      field: 'TACKLING',
+      renderHeader: () => (
+        <div>
+          TA<sup>RE</sup>
+        </div>
+      ),
+      headerAlign: 'center' as GridAlignment,
+      align: 'center' as GridAlignment,
+      minWidth: 50,
+      flex: 1,
+      valueGetter: (params: GridValueGetterParams) => {
+        return params.row.actualSkills.TACKLING?.PlayerSkills.actual || params.row.actualSkills.REFLEXES?.PlayerSkills.actual
+      },
     },
-  },
-  {
-    field: 'CO',
-    headerName: 'CO',
-    headerAlign: 'center' as GridAlignment,
-    align: 'center' as GridAlignment,
-    minWidth: 50,
-    flex: 1,
-    valueGetter: (params: GridValueGetterParams) => params.row.actualSkills.CO?.PlayerSkills.actual,
-  },
-  // {
-  //   field: 'GP',
-  //   headerName: 'GP',
-  //   headerAlign: 'center' as GridAlignment,
-  //   align: 'center' as GridAlignment,
-  //   minWidth: 50,
-  //   flex: 1,
-  //   valueGetter: (params: GridValueGetterParams) => params.row.actualSkills.CO,
-  // },
-  // {
-  //   field: 'GOALS',
-  //   headerName: 'GLs',
-  //   headerAlign: 'center' as GridAlignment,
-  //   align: 'center' as GridAlignment,
-  //   minWidth: 50,
-  //   flex: 1,
-  //   valueGetter: (params: GridValueGetterParams) => params.row.actualSkills.CO,
-  // },
-  // {
-  //   field: 'ASSISTS',
-  //   headerName: 'As',
-  //   headerAlign: 'center' as GridAlignment,
-  //   align: 'center' as GridAlignment,
-  //   minWidth: 50,
-  //   flex: 1,
-  //   valueGetter: (params: GridValueGetterParams) => params.row.actualSkills.CO,
-  // },
-  // {
-  //   field: 'TAKEDOWNS',
-  //   headerName: 'Ta',
-  //   headerAlign: 'center' as GridAlignment,
-  //   align: 'center' as GridAlignment,
-  //   minWidth: 50,
-  //   flex: 1,
-  //   valueGetter: (params: GridValueGetterParams) => params.row.actualSkills.CO,
-  // },
-  // {
-  //   field: 'CRD',
-  //   headerName: 'Crd',
-  //   headerAlign: 'center' as GridAlignment,
-  //   align: 'center' as GridAlignment,
-  //   minWidth: 50,
-  //   flex: 1,
-  //   valueGetter: (params: GridValueGetterParams) => params.row.actualSkills.CO,
-  // },
-  // {
-  //   field: 'MOM',
-  //   headerName: 'MoM',
-  //   headerAlign: 'center' as GridAlignment,
-  //   align: 'center' as GridAlignment,
-  //   minWidth: 50,
-  //   flex: 1,
-  //   valueGetter: (params: GridValueGetterParams) => params.row.actualSkills.CO,
-  // },
-  // {
-  //   field: 'RATING',
-  //   headerName: 'Rating',
-  //   headerAlign: 'center' as GridAlignment,
-  //   align: 'center' as GridAlignment,
-  //   minWidth: 50,
-  //   flex: 1,
-  //   valueGetter: (params: GridValueGetterParams) => params.row.actualSkills.CO,
-  // },
-  // {
-  //   field: 'playerOrder1',
-  //   headerName: 'PO1',
-  //   headerAlign: 'center' as GridAlignment,
-  //   align: 'center' as GridAlignment,
-  //   renderCell: (params: GridCellParams) => <PlayerOrderSelect />,
-  //   minWidth: 70,
-  //   flex: 1,
-  // },
-  // {
-  //   field: 'playerOrder2',
-  //   headerName: 'PO2',
-  //   headerAlign: 'center' as GridAlignment,
-  //   align: 'center' as GridAlignment,
-  //   renderCell: (params: GridCellParams) => <PlayerOrderSelect />,
-  //   minWidth: 70,
-  //   flex: 1,
-  // },
-  // {
-  //   field: 'playerOrder3',
-  //   headerName: 'PO3',
-  //   headerAlign: 'center' as GridAlignment,
-  //   align: 'center' as GridAlignment,
-  //   renderCell: (params: GridCellParams) => <PlayerOrderSelect />,
-  //   minWidth: 70,
-  //   flex: 1,
-  // },
-  // {
-  //   field: 'playerOrder4',
-  //   headerName: 'PO4',
-  //   headerAlign: 'center' as GridAlignment,
-  //   align: 'center' as GridAlignment,
-  //   renderCell: (params: GridCellParams) => <PlayerOrderSelect />,
-  //   minWidth: 70,
-  //   flex: 1,
-  // },
-  // {
-  //   field: 'playerOrder5',
-  //   headerName: 'PO5',
-  //   headerAlign: 'center' as GridAlignment,
-  //   align: 'center' as GridAlignment,
-  //   renderCell: (params: GridCellParams) => <PlayerOrderSelect />,
-  //   minWidth: 70,
-  //   flex: 1,
-  // },
-]
+    {
+      field: 'CO',
+      headerName: 'CO',
+      headerAlign: 'center' as GridAlignment,
+      align: 'center' as GridAlignment,
+      minWidth: 50,
+      flex: 1,
+      valueGetter: (params: GridValueGetterParams) => params.row.actualSkills.CO?.PlayerSkills.actual,
+    },
+    // {
+    //   field: 'GP',
+    //   headerName: 'GP',
+    //   headerAlign: 'center' as GridAlignment,
+    //   align: 'center' as GridAlignment,
+    //   minWidth: 50,
+    //   flex: 1,
+    //   valueGetter: (params: GridValueGetterParams) => params.row.actualSkills.CO,
+    // },
+    // {
+    //   field: 'GOALS',
+    //   headerName: 'GLs',
+    //   headerAlign: 'center' as GridAlignment,
+    //   align: 'center' as GridAlignment,
+    //   minWidth: 50,
+    //   flex: 1,
+    //   valueGetter: (params: GridValueGetterParams) => params.row.actualSkills.CO,
+    // },
+    // {
+    //   field: 'ASSISTS',
+    //   headerName: 'As',
+    //   headerAlign: 'center' as GridAlignment,
+    //   align: 'center' as GridAlignment,
+    //   minWidth: 50,
+    //   flex: 1,
+    //   valueGetter: (params: GridValueGetterParams) => params.row.actualSkills.CO,
+    // },
+    // {
+    //   field: 'TAKEDOWNS',
+    //   headerName: 'Ta',
+    //   headerAlign: 'center' as GridAlignment,
+    //   align: 'center' as GridAlignment,
+    //   minWidth: 50,
+    //   flex: 1,
+    //   valueGetter: (params: GridValueGetterParams) => params.row.actualSkills.CO,
+    // },
+    // {
+    //   field: 'CRD',
+    //   headerName: 'Crd',
+    //   headerAlign: 'center' as GridAlignment,
+    //   align: 'center' as GridAlignment,
+    //   minWidth: 50,
+    //   flex: 1,
+    //   valueGetter: (params: GridValueGetterParams) => params.row.actualSkills.CO,
+    // },
+    // {
+    //   field: 'MOM',
+    //   headerName: 'MoM',
+    //   headerAlign: 'center' as GridAlignment,
+    //   align: 'center' as GridAlignment,
+    //   minWidth: 50,
+    //   flex: 1,
+    //   valueGetter: (params: GridValueGetterParams) => params.row.actualSkills.CO,
+    // },
+    // {
+    //   field: 'RATING',
+    //   headerName: 'Rating',
+    //   headerAlign: 'center' as GridAlignment,
+    //   align: 'center' as GridAlignment,
+    //   minWidth: 50,
+    //   flex: 1,
+    //   valueGetter: (params: GridValueGetterParams) => params.row.actualSkills.CO,
+    // },
+    // {
+    //   field: 'playerOrder1',
+    //   headerName: 'PO1',
+    //   headerAlign: 'center' as GridAlignment,
+    //   align: 'center' as GridAlignment,
+    //   renderCell: (params: GridCellParams) => <PlayerOrderSelect />,
+    //   minWidth: 70,
+    //   flex: 1,
+    // },
+    // {
+    //   field: 'playerOrder2',
+    //   headerName: 'PO2',
+    //   headerAlign: 'center' as GridAlignment,
+    //   align: 'center' as GridAlignment,
+    //   renderCell: (params: GridCellParams) => <PlayerOrderSelect />,
+    //   minWidth: 70,
+    //   flex: 1,
+    // },
+    // {
+    //   field: 'playerOrder3',
+    //   headerName: 'PO3',
+    //   headerAlign: 'center' as GridAlignment,
+    //   align: 'center' as GridAlignment,
+    //   renderCell: (params: GridCellParams) => <PlayerOrderSelect />,
+    //   minWidth: 70,
+    //   flex: 1,
+    // },
+    // {
+    //   field: 'playerOrder4',
+    //   headerName: 'PO4',
+    //   headerAlign: 'center' as GridAlignment,
+    //   align: 'center' as GridAlignment,
+    //   renderCell: (params: GridCellParams) => <PlayerOrderSelect />,
+    //   minWidth: 70,
+    //   flex: 1,
+    // },
+    // {
+    //   field: 'playerOrder5',
+    //   headerName: 'PO5',
+    //   headerAlign: 'center' as GridAlignment,
+    //   align: 'center' as GridAlignment,
+    //   renderCell: (params: GridCellParams) => <PlayerOrderSelect />,
+    //   minWidth: 70,
+    //   flex: 1,
+    // },
+  ]
+
+  if (isEditing)
+    columns.push(
+      {
+        field: 'playerPosition',
+        headerName: 'PP',
+        headerAlign: 'center' as GridAlignment,
+        align: 'center' as GridAlignment,
+        renderCell: () => <PlayerPositionSelect onChange={(value) => console.log(value)} value={PlayerPosition.Forward} />,
+        minWidth: 150,
+        flex: 1,
+      },
+      {
+        field: 'playerOrder',
+        headerName: 'PO',
+        headerAlign: 'center' as GridAlignment,
+        align: 'center' as GridAlignment,
+        renderCell: () => <PlayerOrderSelect />,
+        minWidth: 150,
+        flex: 1,
+      },
+    )
+
+  return columns
+}
---
+++ frontends/beta-frontend/shared/models/PlayerOrder.ts
@@ -0,0 +1,6 @@
+export enum PlayerOrder {
+  PASS_FORWARD = 'passForward',
+  LONG_SHOT = 'longShot',
+  CHANGE_FLANK = 'changeFlank',
+  NONE = 'none',
+}
