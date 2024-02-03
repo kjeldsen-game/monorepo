b2874385c36db2c98b0668f4e18dd2f31c8ca153
kdcr <kevincabrerarodriguez@gmail.com>
2024-02-03 17:07:32 +0000

    Remove unnecessary dependencies from Grid component

--- frontends/beta-frontend/shared/components/Grid/Grid.tsx
+++ frontends/beta-frontend/shared/components/Grid/Grid.tsx
@@ -1,4 +1,4 @@
-import { DataGrid, GridCallbackDetails, GridColDef, GridEventListener, GridRowParams, MuiEvent } from '@mui/x-data-grid'
+import { DataGrid, GridColDef } from '@mui/x-data-grid'
 import Box from '@mui/material/Box'
 
 type GridProps<T> = {
@@ -8,26 +8,27 @@ type GridProps<T> = {
 
 function Grid<T>({ rows, columns }: GridProps<T>) {
   return (
-    <Box sx={{
-      '& .super-app .MuiDataGrid-cellContent': {
-        color: 'white',
-        padding: '6px',
-        fontWeight: '600',
-        borderRadius: '8%',
-      },
-      '& .super-app.goalkeeper .MuiDataGrid-cellContent': {
-        backgroundColor: '#A4BC10',
-      },
-      '& .super-app.defender .MuiDataGrid-cellContent': {
-        backgroundColor: '#F68B29',
-      },
-      '& .super-app.middle .MuiDataGrid-cellContent': {
-        backgroundColor: '#29B6F6',
-      },
-      '& .super-app.forward .MuiDataGrid-cellContent': {
-        backgroundColor: '#E52323',
-      },
-    }}>
+    <Box
+      sx={{
+        '& .super-app .MuiDataGrid-cellContent': {
+          color: 'white',
+          padding: '6px',
+          fontWeight: '600',
+          borderRadius: '8%',
+        },
+        '& .super-app.goalkeeper .MuiDataGrid-cellContent': {
+          backgroundColor: '#A4BC10',
+        },
+        '& .super-app.defender .MuiDataGrid-cellContent': {
+          backgroundColor: '#F68B29',
+        },
+        '& .super-app.middle .MuiDataGrid-cellContent': {
+          backgroundColor: '#29B6F6',
+        },
+        '& .super-app.forward .MuiDataGrid-cellContent': {
+          backgroundColor: '#E52323',
+        },
+      }}>
       <DataGrid autoHeight={true} rows={rows} columns={columns} hideFooter={true} />
     </Box>
   )
