6314a7ee8c11f0ded1f0fab2c696301e3ed1489a
kdcr <kevincabrerarodriguez@gmail.com>
2024-02-25 17:44:28 +0000

    feat(view-other-teams): Disable eslint proptypes rule

--- frontends/beta-frontend/.eslintrc.js
+++ frontends/beta-frontend/.eslintrc.js
@@ -1,27 +1,23 @@
 module.exports = {
-  extends: ["next/core-web-vitals", "next", "prettier", "plugin:react/recommended", "plugin:react-hooks/recommended"],
+  extends: ['next/core-web-vitals', 'next', 'prettier', 'plugin:react/recommended', 'plugin:react-hooks/recommended'],
   plugins: ['react', 'prettier', '@emotion'],
   overrides: [
     {
-        "files": ["*.ts", "*.tsx"],
-        "extends": [
-            "plugin:@typescript-eslint/eslint-recommended",
-            "plugin:@typescript-eslint/recommended",
-            "prettier",
-            "plugin:react/recommended"
-        ],
-        "parser": "@typescript-eslint/parser",
-        "plugins": ["@typescript-eslint"],
-        "rules": {
-            "react/react-in-jsx-scope": "off",
-            "no-shadow": "off",
-            "@typescript-eslint/no-shadow": ["error"],
-            "import/no-unresolved": "off",
-            "react/require-default-props": "off",
-            "no-use-before-define": "off",
-            "@typescript-eslint/no-use-before-define": ["error"],
-            "react/no-unknown-property": ['error', { ignore: ['css'] }]
-        }
-    }
-]
+      files: ['*.ts', '*.tsx'],
+      extends: ['plugin:@typescript-eslint/eslint-recommended', 'plugin:@typescript-eslint/recommended', 'prettier', 'plugin:react/recommended'],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint'],
+      rules: {
+        'react/react-in-jsx-scope': 'off',
+        'no-shadow': 'off',
+        '@typescript-eslint/no-shadow': ['error'],
+        'import/no-unresolved': 'off',
+        'react/require-default-props': 'off',
+        'no-use-before-define': 'off',
+        '@typescript-eslint/no-use-before-define': ['error'],
+        'react/no-unknown-property': ['error', { ignore: ['css'] }],
+        'react/prop-types': 'off',
+      },
+    },
+  ],
 }
