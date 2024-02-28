b8a725d5cce625ecd5609435901561916abf8141
kdcr <kevincabrerarodriguez@gmail.com>
2024-02-28 20:15:16 +0000

    feat(view-other-teams): Add pagination to league table

--- frontends/beta-frontend/pages/league/components/LeagueGrid.tsx
+++ frontends/beta-frontend/pages/league/components/LeagueGrid.tsx
@@ -2,13 +2,27 @@ import { useAllTeamsRepository } from '../../api/team/useAllTeamsRepository'
 import Grid from '@/shared/components/Grid/Grid'
 import { Box } from '@mui/material'
 import { leagueColumns } from './LeagueColumns'
+import { useState } from 'react'
+
+const PAGE_SIZE = 10
 
 const LeagueGrid: React.FC = () => {
-  const { allTeams } = useAllTeamsRepository(0, 10)
+  const [selectedPage, setSelectedPage] = useState<number>(0)
+  const { allTeams } = useAllTeamsRepository(selectedPage, PAGE_SIZE)
 
   return (
     <Box sx={{ width: '100%' }}>
-      <Grid rows={allTeams ?? []} columns={leagueColumns} />
+      <Grid
+        rows={allTeams ?? []}
+        columns={leagueColumns}
+        paginationMode="server"
+        pagination
+        pageSize={PAGE_SIZE}
+        hideFooter={false}
+        onPageChange={(value) => setSelectedPage(value)}
+        // TODO: Get total of teams from the API
+        rowCount={10}
+      />
     </Box>
   )
 }
