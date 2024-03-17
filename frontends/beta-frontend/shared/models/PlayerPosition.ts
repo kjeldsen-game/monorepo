9a4e7973359126a644f3e752a1574a20ca344cd8
kdcr <kevincabrerarodriguez@gmail.com>
2024-03-17 18:09:27 +0000

    feat(challenge-ui): Update player position colors

--- frontends/beta-frontend/shared/models/PlayerPosition.ts
+++ frontends/beta-frontend/shared/models/PlayerPosition.ts
@@ -21,23 +21,23 @@ export enum PlayerPosition {
 }
 
 export enum PlayerPositionColor {
-  CENTRE_BACK = '#000000',
-  AERIAL_CENTRE_BACK = '#333333',
-  SWEEPER = '#666666',
-  LEFT_BACK = '#999999',
-  RIGHT_BACK = '#CCCCCC',
-  LEFT_WINGBACK = '#FF0000',
-  RIGHT_WINGBACK = '#00FF00',
-  DEFENSIVE_MIDFIELDER = '#0000FF',
-  CENTRE_MIDFIELDER = '#FFFF00',
-  LEFT_MIDFIELDER = '#00FFFF',
-  RIGHT_MIDFIELDER = '#FF00FF',
-  LEFT_WINGER = '#FF9900',
-  OFFENSIVE_MIDFIELDER = '#9900FF',
-  RIGHT_WINGER = '#00FF99',
-  FORWARD = '#FFCC00',
-  AERIAL_FORWARD = '#00CCFF',
-  STRIKER = '#CC00FF',
-  AERIAL_STRIKER = '#FF00CC',
-  GOALKEEPER = '#A4BC10',
+  CENTRE_BACK = '#594fc8',
+  AERIAL_CENTRE_BACK = '#6d6394', // Mezcla más clara de #090446 y #786f52
+  SWEEPER = '#f7d197', // Desaturación y cambio de luminosidad de #FEB95F
+  LEFT_BACK = '#d14d6f', // Mezcla más clara de #f71735 y #c2095a
+  RIGHT_BACK = '#cc7da8', // Mezcla más clara de #c2095a y #786f52
+  LEFT_WINGBACK = '#5971a6', // Mezcla más clara de #090446 y #f71735
+  RIGHT_WINGBACK = '#a8907d', // Mezcla más clara de #786f52 y #c2095a
+  DEFENSIVE_MIDFIELDER = '#fbd6a4', // Desaturación y cambio de luminosidad de #FEB95F
+  CENTRE_MIDFIELDER = '#e75c7b', // Mezcla más clara de #f71735 y #c2095a
+  LEFT_MIDFIELDER = '#985377', // Mezcla más clara de #c2095a y #090446
+  RIGHT_MIDFIELDER = '#867e5b', // Mezcla más clara de #090446 y #786f52
+  LEFT_WINGER = '#43d9be', // Completamente diferente a los colores de referencia
+  OFFENSIVE_MIDFIELDER = '#a7608e', // Mezcla más clara de #f71735 y #786f52
+  RIGHT_WINGER = '#fda4a0', // Mezcla más clara de #c2095a y #f71735
+  FORWARD = '#3a9db9', // Mezcla más clara de #c2095a y #090446
+  AERIAL_FORWARD = '#8497bd', // Mezcla más clara de #090446 y #786f52
+  STRIKER = '#fddfb1', // Desaturación y cambio de luminosidad de #FEB95F
+  AERIAL_STRIKER = '#b7738e', // Mezcla más clara de #c2095a y #f71735
+  GOALKEEPER = '#e1798e', // Mezcla más clara de #f71735 y #c2095a
 }
