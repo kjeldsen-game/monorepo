a5897d346c25c8ecc6687694ed9d6ca663174852
kdcr <kevincabrerarodriguez@gmail.com>
2024-03-24 17:33:54 +0000

    feat(challenge-ui): Update player matches repo hook

--- frontends/beta-frontend/shared/models/Match.ts
+++ frontends/beta-frontend/shared/models/Match.ts
@@ -1,8 +1,12 @@
 export interface Match {
+  id: string
   home: MatchTeam
   away: MatchTeam
   dateTime: Date
+  status: MatchStatus
 }
+
+export type MatchStatus = 'PENDING' | 'ACCEPTED' | 'REJECTED'
 
 export interface MatchTeam {
   id: string
