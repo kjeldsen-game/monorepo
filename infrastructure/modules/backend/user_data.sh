2d11178a1cd0a65e4502804f87c8b6f0f364523c
Ivan <ivangonzalezcabral94@gmail.com>
2024-11-17 15:16:44 +0100

    added database module

--- infrastructure/modules/backend/user_data.sh
+++ infrastructure/modules/backend/user_data.sh
@@ -15,18 +15,6 @@ echo "ec2-user:${ssh_password}" | chpasswd
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 service sshd restart
 
-# Download DocumentDB certificate
-mkdir -p /home/ec2-user/certs
-wget -O /home/ec2-user/certs/rds-combined-ca-bundle.pem https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem
-
-#Add the DocumentDB certificate to a Java truststore
-yum install -y java-17-amazon-corretto
-keytool -import -trustcacerts \
-  -file /home/ec2-user/certs/rds-combined-ca-bundle.pem \
-  -alias docdb \
-  -keystore /home/ec2-user/certs/truststore.jks \
-  -storepass ${ssh_password} -noprompt
-
 # Create environment file
 cat << EOF > /home/ec2-user/.env
 %{ for key, value in environment_vars ~}
@@ -43,6 +31,5 @@ docker run -d \
   --name backend \
   --restart always \
   -p ${container_external_port}:${container_internal_port} \
-  -v /home/ec2-user/certs:/certs \
   --env-file /home/ec2-user/.env \
   ${docker_image} 
